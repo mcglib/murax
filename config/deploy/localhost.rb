61f2febb3c266d2d8a18e61695f80a24f28291da
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2018-10-23 12:07:21 -0400

    Updated

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,5 +1,5 @@
 # For use in first ansible deploy
 set :stage, :localhost
-set :rails_env, 'production'
+set :rails_env, 'productions'
 set :deploy_to, '/storage/www/murax/current'
 server '127.0.0.1', user: 'dev.library', roles: [:web, :app, :db]
