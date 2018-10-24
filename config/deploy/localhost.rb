caf8939a100226feb53b6b9f66b67731e4723dd5
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2018-10-24 12:25:47 -0400

    Tweaking the cap file

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -2,4 +2,5 @@
 set :stage, :localhost
 set :rails_env, 'production'
 set :deploy_to, '/storage/www/murax'
+set :ssh_options, keys: ['id_rsa'] if File.exist?('id_rsa')
 server '127.0.0.1', user: 'dev.library', roles: [:web, :app, :db]
