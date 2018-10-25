038db93ee12cfdf31ceb896b3846f6b3b374c1a0
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2018-10-25 12:08:01 -0400

    Disabling running sidekiq restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,26 +46,26 @@ set :keep_releases, 5
 # We have to re-define capistrano-sidekiq's tasks to work with
 # systemctl in production. Note that you must clear the previously-defined
 # tasks before re-defining them.
-Rake::Task["sidekiq:stop"].clear_actions
-Rake::Task["sidekiq:start"].clear_actions
-Rake::Task["sidekiq:restart"].clear_actions
-namespace :sidekiq do
-  task :stop do
-    on roles(:app) do
-      execute :sudo, :systemctl, :stop, :sidekiq
-    end
-  end
-  task :start do
-    on roles(:app) do
-      execute :sudo, :systemctl, :start, :sidekiq
-    end
-  end
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :systemctl, :restart, :sidekiq
-    end
-  end
-end
+#Rake::Task["sidekiq:stop"].clear_actions
+#Rake::Task["sidekiq:start"].clear_actions
+#Rake::Task["sidekiq:restart"].clear_actions
+#namespace :sidekiq do
+#  task :stop do
+#    on roles(:app) do
+#      execute :sudo, :systemctl, :stop, :sidekiq
+#    end
+#  end
+#  task :start do
+#    on roles(:app) do
+#      execute :sudo, :systemctl, :start, :sidekiq
+#    end
+#  end
+#  task :restart do
+#    on roles(:app) do
+#      execute :sudo, :systemctl, :restart, :sidekiq
+#    end
+#  end
+#end
 
 # Capistrano passenger restart isn't working consistently,
 # so restart apache2 after a successful deploy, to ensure
