fdd6b33e258918aacfd3669c49181d14f87aba9c
Mutugi Gathuri <mutugi.gathuri@mcgill.ca>
2018-11-01 16:15:27 -0400

    Enabled the browse everything engine

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,5 +1,5 @@
 # For use in production
-set :stage, :localhost
+set :stage, :production
 set :rails_env, 'production'
 set :deploy_to, '/storage/www/murax'
 set :ssh_options, keys: ['id_rsa'] if File.exist?('id_rsa')
