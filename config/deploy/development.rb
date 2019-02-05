b6523e62376d61bef8e9fbef792cbc6dc199486e
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-02-05 09:18:00 -0500

    Updated the code

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,5 +2,7 @@
 set :stage, :development
 set :rails_env, 'production'
 set :deploy_to, '/storage/www/murax'
+set :repository_cache, "git_cache"
+set :branch, "develop"
 set :ssh_options, keys: ['id_rsa'] if File.exist?('id_rsa')
 server 'dlirap.library.mcgill.ca', user: 'dev.library', roles: [:web, :app, :db]
