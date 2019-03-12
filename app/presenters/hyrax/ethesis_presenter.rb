11548137650b6eeee5d9c13bdd8db6b1c69577fb
Awais Mehmood Khalid <mehmood.khalid@mcgill.ca>
2019-03-12 14:05:19 -0400

    Changed metadata again for the changes

--- app/presenters/hyrax/ethesis_presenter.rb
+++ app/presenters/hyrax/ethesis_presenter.rb
@@ -2,7 +2,7 @@
 #  `rails generate hyrax:work Ethesis`
 module Hyrax
   class EthesisPresenter < Hyrax::WorkShowPresenter
-    delegate :abstract,  :alternative_title, :Description,
+    delegate :abstract,  :alternative_title, :description,
              :degree, :institution, :department,
              :subject, :language_label, :license_label, :note, :place_of_publication,
              :use, to: :solr_document
