5e4bfed133bcbaa7cd2d7e85dfcc0db524ab61c3
Awais Mehmood Khalid <mehmood.khalid@mcgill.ca>
2019-03-18 15:58:33 -0400

    created a new work type Thesis

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -13,6 +13,8 @@ Hyrax.config do |config|
   config.register_curation_concern :poster
   # Injected via `rails g hyrax:work Book`
   config.register_curation_concern :book
+  # Injected via `rails g hyrax:work Thesis`
+  config.register_curation_concern :thesis
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
