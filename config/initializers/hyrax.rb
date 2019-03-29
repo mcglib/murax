e3e4af63ea6d0671fcd629623c2fd22f17e96399
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-03-29 15:12:31 -0400

    Removing the ethesis worktype artifacts

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -5,8 +5,6 @@ Hyrax.config do |config|
   config.register_curation_concern :image
   # Injected via `rails g hyrax:work Work`
   config.register_curation_concern :work
-  # Injected via `rails g hyrax:work Ethesis`
-  config.register_curation_concern :ethesis
   # Injected via `rails g hyrax:work Article`
   config.register_curation_concern :article
   # Injected via `rails g hyrax:work Poster`
