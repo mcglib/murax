5404d4e35d89beccd45ebf3b6617822778e478e3
Awais Mehmood Khalid <mehmood.khalid@mcgill.ca>
2019-04-15 10:47:48 -0400

    added relation labels

--- app/presenters/hyrax/thesis_presenter.rb
+++ app/presenters/hyrax/thesis_presenter.rb
@@ -4,7 +4,7 @@ module Hyrax
   class ThesisPresenter < Hyrax::WorkShowPresenter
     delegate :abstract,  :alternative_title, :description,
              :degree,:department, :degree, :faculty, :rights,:rtype,:date,:institution, :orcidid,
-             :subject, :license_label, :note, :place_of_publication, :language,
+             :subject, :license_label, :note, :place_of_publication, :language, :relation,
              :use, to: :solr_document
   end
 end
