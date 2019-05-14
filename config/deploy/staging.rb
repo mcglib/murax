15923da68a85406495fffd4587af2b70f4c09d6c
Awais Mehmood Khalid <mehmood.khalid@mcgill.ca>
2019-05-14 16:14:47 -0400

    changed deploy branch to develop in murax/config/deploy/staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,6 +3,6 @@ set :stage, :staging
 set :rails_env, 'production'
 set :deploy_to, '/storage/www/murax'
 set :repository_cache, "git_cache"
-set :branch, "master"
+set :branch, "develop"
 set :ssh_options, keys: ['id_rsa'] if File.exist?('id_rsa')
 server 'qlirap.library.mcgill.ca', user: 'dev.library', roles: [:web, :app, :db]
