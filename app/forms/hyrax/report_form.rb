4beef844bd2fc51f7e1f232b1d42078254e6b50f
Mehmood <mehmood@lso93.library.mcgill.ca>
2019-06-14 14:07:06 -0400

    changed some config for license

--- app/forms/hyrax/report_form.rb
+++ app/forms/hyrax/report_form.rb
@@ -7,11 +7,11 @@ module Hyrax
     self.model_class = ::Report
         self.terms += [:title, :faculty, :alternative_title, :rights, :local_affiliated_centre, :department,
                    :creator, :note,:abstract, :research_unit, :grant_number, :degree,
-                   :date,  :rights, :rtype, :orcidid, :relation, :report_number, :pmid ]
+                   :date,  :rights, :rtype, :orcidid, :relation, :report_number, :pmid, :license ]
     self.terms -= [ :keyword, :rights_statement,:status, :date_created,:bibliographic_citation, :resource_type,:import_url, :relative_path, :based_near]
     self.required_fields += [:creator, :date, :rights, :rtype]
     self.single_valued_fields = [:alternative_title, :creator, :contributor, :description, :abstract, :note, 
-                                 :date, :source, :department,
+                                 :date, :source, :department, :license,
                                  :faculty, :rights, :orcidid, :related_url]
   end
 end
