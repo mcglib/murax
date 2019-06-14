c4b9d6342d0fb046f1bf55b94dba1e249bb949ce
Mehmood <mehmood@lso93.library.mcgill.ca>
2019-06-14 11:06:01 -0400

    Created the workptype Report

---
+++ app/forms/hyrax/report_form.rb
@@ -0,0 +1,17 @@
+# Generated via
+#  `rails generate hyrax:work Report`
+module Hyrax
+  # Generated form for Report
+  class ReportForm < Hyrax::Forms::WorkForm
+    include SingleValuedForm
+    self.model_class = ::Report
+        self.terms += [:title, :faculty, :alternative_title, :rights, :local_affiliated_centre, :department,
+                   :creator, :note,:abstract, :research_unit, :grant_number, :degree,
+                   :date,  :rights, :rtype, :orcidid, :relation, :report_number, :pmid ]
+    self.terms -= [ :keyword, :rights_statement,:status, :date_created,:bibliographic_citation, :resource_type,:import_url, :relative_path, :based_near]
+    self.required_fields += [:creator, :date, :rights, :rtype]
+    self.single_valued_fields = [:alternative_title, :creator, :contributor, :description, :abstract, :note, 
+                                 :publisher, :date, :report_number, :source, :department,
+                                 :faculty, :rights, :orcidid, :related_url, :relation]
+  end
+end
