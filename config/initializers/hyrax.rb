c3c7020372e7e8411b195d98d7bd965becb1187e
Mehmood <mehmood@ICSLAPSs-MacBook-Pro.local>
2019-07-24 11:49:17 -0400

    Removing worktypes IMAGE and POSTER

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -1,14 +1,14 @@
 Hyrax.config do |config|
   # Injected via `rails g hyrax:work GenericWork`
   config.register_curation_concern :generic_work
-  # Injected via `rails g hyrax:work Image`
-  config.register_curation_concern :image
+  ## # Injected via `rails g hyrax:work Image`
+  ## # config.register_curation_concern :image
   # Injected via `rails g hyrax:work Work`
   config.register_curation_concern :work
   # Injected via `rails g hyrax:work Article`
   config.register_curation_concern :article
-  # Injected via `rails g hyrax:work Poster`
-  config.register_curation_concern :poster
+  ## # Injected via `rails g hyrax:work Poster`
+  ## # config.register_curation_concern :poster
   # Injected via `rails g hyrax:work Book`
   config.register_curation_concern :book
   # Injected via `rails g hyrax:work Thesis`
