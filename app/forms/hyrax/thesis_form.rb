ff20834c6d44eaee73fedef1786fa5dcf7904e7a
Mehmood <mehmood@ICSLAPSs-MacBook-Pro.local>
2019-07-24 12:33:54 -0400

    Changed the proprty fields as requested again in ADIR-426

--- app/forms/hyrax/thesis_form.rb
+++ app/forms/hyrax/thesis_form.rb
@@ -3,15 +3,15 @@
 module Hyrax
   # Generated form for Thesis
   class ThesisForm < Hyrax::Forms::WorkForm
-    class_attribute :single_value_fields
+    include SingleValuedForm
     self.model_class = ::Thesis
     self.terms += [:title, :degree, :institution, :faculty,:alternative_title, :rights,
-                   :creator, :note, :publisher, :extent, :abstract, 
+                   :creator, :note, :extent, :abstract, :department,
                    :date,  :rights, :subject, :rtype, :orcidid,  :identifier, :relation ]
     self.terms -= [ :keyword, :rights_statement, :date_created, :source, :resource_type, :bibliographic_citation, :import_url, :relative_path, :based_near]
+    self.required_fields += [:creator,:date, :subject, :rights, :rtype, :identifier, :department]
+    self.required_fields -= [:keyword, :contact_email, :description, :faculty]
+    self.single_valued_fields = [:title, :rtype]
 
-    self.required_fields += [:creator, :abstract,  :publisher, :date, :subject, :institution, :degree,
-                            :rights, :rtype, :identifier]
-    self.required_fields -= [:keyword, :contact_email, :description, :faculty, :department]
   end
 end
