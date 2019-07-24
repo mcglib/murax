685cb2c85b1a25da2aa3df7cdf339960bdc39b34
Mehmood <mehmood@ICSLAPSs-MacBook-Pro.local>
2019-07-24 12:43:41 -0400

    Changed the proprty fields as requested again in ADIR-433

--- app/forms/hyrax/article_form.rb
+++ app/forms/hyrax/article_form.rb
@@ -6,13 +6,11 @@ module Hyrax
     include SingleValuedForm
     self.model_class = ::Article
     self.terms += [:title, :faculty, :alternative_title, :rights, :local_affiliated_centre, :department,
-                   :creator, :note, :publisher, :abstract, :pmid, :research_unit, :grant_number, :status,
-                   :date,  :rights, :rtype, :orcidid,  :identifier, :relation, :bibliographic_citation ]
+                   :creator, :note, :abstract, :pmid, :research_unit, :grant_number, :status, :extent,
+                   :date,  :rights, :rtype, :orcidid, :relation, :bibliographic_citation ]
     self.terms -= [ :keyword, :rights_statement, :date_created, :resource_type,:import_url, :relative_path, :based_near]
     self.required_fields += [:creator, :date, :rights, :rtype, :bibliographic_citation]
     self.required_fields -= [:keyword, :contact_email, :description]
-    self.single_valued_fields = [:title, :bibliographic_citation]
-
-
+    self.single_valued_fields = [:title, :extent]
   end
 end
