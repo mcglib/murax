1100caabf8606c6bf8e8ba84005a0f89826d1679
Mehmood <mehmood@ICSLAPSs-MacBook-Pro.local>
2019-07-30 11:59:56 -0400

    modification to thesis form

--- app/forms/hyrax/thesis_form.rb
+++ app/forms/hyrax/thesis_form.rb
@@ -5,8 +5,8 @@ module Hyrax
   class ThesisForm < Hyrax::Forms::WorkForm
     include SingleValuedForm
     self.model_class = ::Thesis
-    self.terms += [:title, :degree, :institution, :faculty,:alternative_title, :rights,
-                   :creator, :note, :extent, :abstract, :department,
+    self.terms += [:title, :degree, :institution, :faculty, :rights,
+                   :creator, :note, :extent, :abstract, :department, :source, 
                    :date,  :rights, :subject, :rtype, :orcidid,  :identifier, :relation ]
     self.terms -= [ :keyword, :rights_statement, :date_created, :source, :resource_type, :bibliographic_citation, :import_url, :relative_path, :based_near]
     self.required_fields += [:creator,:date, :subject, :rights, :rtype, :identifier, :department]
