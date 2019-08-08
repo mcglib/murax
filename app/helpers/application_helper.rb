10e99643dff1e9af06e600595b08250ece4ffb98
Git Library <dev.library@dgeneral-2.library.mcgill.ca>
2019-08-08 16:36:20 -0400

    created two helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,9 +6,23 @@ module ApplicationHelper
       item_type = item_types["Article"]
     elsif request.path.starts_with?('/concern/theses')
       item_type = item_types["Thesis"]
+    elsif request.path.starts_with?('/concern/books')
+      item_type = item_types["Book"]
+    elsif request.path.starts_with?('/concern/papers')
+      item_type = item_types["Paper"]
+    elsif request.path.starts_with?('/concern/presentations')
+      item_type = item_types["Presentation"]
     else 
       item_type = item_types["Thesis"]
     end
+    return item_type
+  end
+
+  def select_type
+    item_url = request.original_fullpath
+    item_split = item_url.split('/')
+    item_type = item_split[2]
+    item_type = item_type.capitalize.singularize
     return item_type
   end 
 end
