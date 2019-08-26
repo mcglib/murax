8f0784d82244daecb42253642d84879f9a48deec
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-08-26 12:05:35 -0400

    Removed debugging code

--- app/services/attach_work_to_collection.rb
+++ app/services/attach_work_to_collection.rb
@@ -2,7 +2,6 @@ class AttachWorkToCollection
   def self.call(work_id, work_type, collectionObj)
       attached = true
       # Get the work
-      byebug
       work = (work_type.singularize.classify.constantize).find(work_id)
 
       begin
