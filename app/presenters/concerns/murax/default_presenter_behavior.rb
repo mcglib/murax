9750835cd71101f6d239ad2f07f268b30aafd787
Awais Mehmood Khalid <mehmood.khalid@mcgill.ca>
2019-09-04 14:28:46 -0400

    fixed the author order for book work-type, needed modifications were 1) app/forms/hyrax/book_form.rb 2) app/models/book.rb 3) app/presenters/concerns/murax/default_presenter_behavior.rb 4) app/presenters/hyrax/book_presenter.rb, better organizing will be done in murax version 2

--- app/presenters/concerns/murax/default_presenter_behavior.rb
+++ app/presenters/concerns/murax/default_presenter_behavior.rb
@@ -6,7 +6,7 @@ module Murax
     extend ActiveSupport::Concern
     included do
       delegate :nested_ordered_creator, :nested_ordered_creator_label,
-               :title, :alternative_title, :contributor, :local_affiliated_centre, :department, :subject, :extent,
+               :title, :alternative_title, :contributor, :local_affiliated_centre, :department, :subject, :extent, :license,
                :note, :publisher, :abstract, :pmid, :research_unit, :grant_number, :status, :description, :source, :language,
                :date,  :rights, :rtype, :orcidid, :identifier, :bibliographic_citation, :relation, :report_number, :related_url, :faculty, :degree, :author_order,
                :use,  to: :solr_document
