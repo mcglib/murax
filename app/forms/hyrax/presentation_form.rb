85cdc74dafa3e672a537d5787d861c36a2ee3909
Awais Mehmood Khalid <mehmood.khalid@mcgill.ca>
2019-09-04 15:00:13 -0400

    fixed the author order for presentaion work-type, needed modifications were 1) app/forms/hyrax/presentation_form.rb 2) app/models/presentation.rb 3) app/presenters/concerns/murax/default_presenter_behavior.rb 4) app/presenters/hyrax/presentation_presenter.rb, better organizing will be done in murax version 2

--- app/forms/hyrax/presentation_form.rb
+++ app/forms/hyrax/presentation_form.rb
@@ -4,13 +4,26 @@ module Hyrax
   # Generated form for Presentation
   class PresentationForm < Hyrax::Forms::WorkForm
     include SingleValuedForm
+    include Hyrax::NestedBehavior
     self.model_class = ::Presentation
-    self.terms += [:title,:alternative_title, :creator,:local_affiliated_centre, :department, :extent, 
+    self.terms += [:title,:alternative_title, :nested_ordered_creator,:local_affiliated_centre, :department, :extent, 
                    :note, :abstract, :pmid, :research_unit, :grant_number,
                    :date,  :rights, :rtype, :orcidid, :relation,:faculty, :degree, :author_order ]
-    self.terms -= [ :keyword, :rights_statement, :date_created, :resource_type,:import_url, :relative_path, :based_near]
-    self.required_fields += [:creator, :date, :rights, :rtype]
+    self.terms -= [ :keyword, :creator, :rights_statement, :date_created, :resource_type,:import_url, :relative_path, :based_near]
+    self.required_fields += [:nested_ordered_creator, :date, :rights, :rtype]
     self.required_fields -= [:keyword, :contact_email, :description, :rights_statement]
     self.single_valued_fields = [:title, :rtype]
+
+    def primary_terms
+      [:title, :nested_ordered_creator, :date, :rights, :rtype ] | super
+    end
+
+    def self.build_permitted_params
+      super + [
+        {
+          nested_ordered_creator_attributes: %i[id _destroy index creator],
+        }
+      ]
+    end
   end
 end
