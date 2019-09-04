a13a01a58f6972ce65cae59c3cb550b334f78750
Awais Mehmood Khalid <mehmood.khalid@mcgill.ca>
2019-09-04 10:38:45 -0400

    organized code

---
+++ app/presenters/hyrax/default_presenter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Hyrax
+  class DefaultPresenter < Hyrax::WorkShowPresenter
+    include Murax::DefaultPresenterBehavior
+  end
+end
+
---
+++ app/presenters/hyrax/report_presenter.rb
@@ -0,0 +1,6 @@
+# Generated via
+#  `rails generate hyrax:work Report`
+module Hyrax
+  class ReportPresenter < DefaultPresenter
+  end
+end
