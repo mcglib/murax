1db6092ae63bd5e2ca876a13828e8e46532450fd
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-09-16 08:40:22 -0400

    Adding date accepted

--- app/models/digitool/thesis_item.rb
+++ app/models/digitool/thesis_item.rb
@@ -100,8 +100,6 @@ class Digitool::ThesisItem < DigitoolItem
         work_attributes['nested_ordered_creator_attributes'] << process_ordered_field("creator", term.text, index) unless term.text.nil?
       end
 
-
-
       # Set the abstract
       work_attributes['abstract'] = set_abstracts(xml.xpath("/record/abstract"))
 
@@ -127,6 +125,11 @@ class Digitool::ThesisItem < DigitoolItem
         date_modified =  DateTime.strptime(date_modified_string, '%m/%d/%Y')
                         .strftime('%Y-%m-%d')
         work_attributes['date_created'] =  [date_modified.to_s]
+      end
+      # set the disaccpetdate
+      work_attributes['date_accepted'] =  []
+      xml.xpath("/record/localdissacceptdate").each do |term|
+        work_attributes['date_accepted'] << term.text if term.text.present?
       end
 
 
