991e235d4b3bb41f89456e217c51386af94fd1c8
Mehmood <mehmood@ICSLAPSs-MacBook-Pro.local>
2019-10-29 09:29:15 -0400

    added hook on files controller to send an email out when a file is deleted.

--- app/mailers/work_delete_mailer.rb
+++ app/mailers/work_delete_mailer.rb
@@ -2,6 +2,7 @@ class WorkDeleteMailer < ApplicationMailer
   default from: ENV['ADMIN_EMAIL'],
           cc: ENV['ADMIN_EMAIL']
 
+  # An email that goes out when a work is deleted in the repository. 
   def work_delete_email
     @user = params[:user]
     @deleted_work_title = params[:deleted_work_title]
@@ -11,4 +12,16 @@ class WorkDeleteMailer < ApplicationMailer
     )
   end
 
+
+  # An email that goes out when file/s are deleted in the repository.
+  def file_delete_email
+    @user = params[:user]
+    @deleted_files_work_title = params[:deleted_files_work_title]
+    @deleted_files_work_id = params[:deleted_files_work_id]
+    @deleted_files = params[:deleted_file_name]
+    @deleted_file_id = params[:deleted_file_id]
+    mail(to: ENV['CONTACT_US_EMAIL'],
+      subject: "A file has been deleted in  #{ENV['RAILS_HOST']}"
+    )
+  end
 end
