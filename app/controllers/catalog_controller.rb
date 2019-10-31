d3415353e1d53b67dc27aec642d5df02b617990d
Git Library <dev.library@dgeneral-2.library.mcgill.ca>
2019-10-31 15:27:10 -0400

    fixed the thesis link in search results

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -99,7 +99,8 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name("rtype", :stored_searchable), label: "Type", link_to_search: solr_name("rtype", :facetable)
    # config.add_index_field solr_name("resource_type", :stored_searchable), label: "Resource Type", link_to_search: solr_name("resource_type", :facetable) -- removing to replace it with rtype.
     config.add_index_field solr_name("file_format", :stored_searchable), link_to_search: solr_name("file_format", :facetable)
-    config.add_index_field solr_name("identifier", :stored_searchable), helper_method: :index_field_link, field_name: 'identifier'
+    #config.add_index_field solr_name("identifier", :stored_searchable), helper_method: :index_field_link, field_name: 'identifier'
+    config.add_index_field solr_name("identifier", :stored_searchable), helper_method: :iconify_auto_link, field_name: 'identifier'
     config.add_index_field solr_name("embargo_release_date", :stored_sortable, type: :date), label: "Embargo release date", helper_method: :human_readable_date
     config.add_index_field solr_name("lease_expiration_date", :stored_sortable, type: :date), label: "Lease expiration date", helper_method: :human_readable_date
 
