32de9ea07ad33cc868d8f1143b1bac93bd4df273
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-11-28 08:35:16 -0500

    Renaming the service to be more generic

--- app/services/murax/digitool_service.rb
+++ app/services/murax/digitool_service.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 module Murax
-  # DigitoolXml Service
-  class DigitoolXmlService
+  # Digitool Service
+  class DigitoolService
     def self.get_xml_by_workid(workid)
-      # check that the workid exists
        xml = nil
        begin
           # Lets load the import log for this workid
@@ -16,6 +15,20 @@ module Murax
        end
 
        xml
+    end
+    def self.get_pid_by_workid(workid)
+       pid = nil
+       begin
+          # Lets load the import log for this workid
+         import_log = ImportLog.where(work_id: workid).first
+         pid = import_log.raw_xml if import_log.present?
+       rescue ActiveRecord::RecordNotFound
+         puts "Couldn't find the xml for the #{workid}. Missing the log record"
+       rescue ActiveRecord::ActiveRecordError
+         puts "Error occured fetching the xml"
+       end
+
+       pid
     end
 
     private
