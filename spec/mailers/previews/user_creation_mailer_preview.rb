1f1264e6a672671325cedfaf04a67a62679c6267
Mk <mehmood.khalid@mcgill.ca>
2020-01-17 15:05:32 -0500

    fixed a typo

--- spec/mailers/previews/user_creation_mailer_preview.rb
+++ spec/mailers/previews/user_creation_mailer_preview.rb
@@ -1,5 +1,5 @@
 class UserCreationMailerPreview < ActionMailer::Preview
-  def work_delete_email
+  def user_password_email
     UserCreationMailer.with(user_email: 'u.email', user_name: 'u.display_name', user_password: 'generated_password', assigned_role: 'existing_role').user_password_email
   end
 
