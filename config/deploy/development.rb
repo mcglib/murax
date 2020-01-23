73c168bb5d9db1d44421490eed65644e069135a6
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2020-01-23 08:17:30 -0500

    testing adding development gems into dlirap

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,5 +3,6 @@ set :stage, :production
 set :rails_env, 'production'
 set :repository_cache, "git_cache"
 set :branch, "develop"
+set :bundle_without, nil
 set :ssh_options, keys: ['id_rsa'] if File.exist?('id_rsa')
 server 'dlirap.library.mcgill.ca', user: 'dev.library', roles: [:web, :app, :db]
