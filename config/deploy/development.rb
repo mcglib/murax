611e5103aa4d5d171d3de903f4d73a7e7335b75a
Mutugi Gathuri <mutugi.gathuri@mcgill.ca>
2020-01-24 15:21:42 -0500

    disabling use of bundler without

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,6 +3,5 @@ set :stage, :production
 set :rails_env, 'production'
 set :repository_cache, "git_cache"
 set :branch, "develop"
-set :bundle_without, nil
 set :ssh_options, keys: ['id_rsa'] if File.exist?('id_rsa')
 server 'dlirap.library.mcgill.ca', user: 'dev.library', roles: [:web, :app, :db]
