157f2becd0b3bade579f8e37266f63dfbced3ba6
Mutugi Gathuri <mutugi.gathuri@mcgill.ca>
2020-03-10 09:22:11 -0400

    Bumped to v1.1.24

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -114,11 +114,11 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name("contributor", :stored_searchable), itemprop: 'contributor', link_to_search: solr_name("contributor", :facetable)
     config.add_index_field solr_name("title", :stored_searchable), label: "Title", itemprop: 'name', if: false
     config.add_index_field solr_name("description", :stored_searchable), itemprop: 'description', helper_method: :iconify_auto_link, label: "Description"
-    config.add_index_field solr_name("abstract", :stored_searchable), itemprop: 'abstract', label: "Abstract", highlight: true
-   # config.add_index_field solr_name("relation", :stored_searchable), itemprop: 'relation', link_to_search: solr_name("relation", :facetable), label: "Relation"
+    # config.add_index_field solr_name("abstract", :stored_searchable), itemprop: 'abstract', label: "Abstract"
+    # config.add_index_field solr_name("relation", :stored_searchable), itemprop: 'relation', link_to_search: solr_name("relation", :facetable), label: "Relation"
     config.add_index_field solr_name("subject", :stored_searchable), itemprop: 'about', link_to_search: solr_name("subject", :facetable)
     config.add_index_field solr_name("proxy_depositor", :symbol), label: "Depositor", helper_method: :link_to_profile
-   # config.add_index_field solr_name("depositor"), label: "Owner", helper_method: :link_to_profile
+    # config.add_index_field solr_name("depositor"), label: "Owner", helper_method: :link_to_profile
     config.add_index_field solr_name("publisher", :stored_searchable), itemprop: 'publisher', link_to_search: solr_name("publisher", :facetable)
     config.add_index_field solr_name("based_near_label", :stored_searchable), itemprop: 'contentLocation', link_to_search: solr_name("based_near_label", :facetable)
     config.add_index_field solr_name("language", :stored_searchable), itemprop: 'inLanguage', link_to_search: solr_name("language", :facetable), helper_method: :language_links
@@ -130,7 +130,7 @@ class CatalogController < ApplicationController
     config.add_index_field solr_name("rights", :stored_searchable), link_to_search: solr_name("rights"), label: "Rights"
     config.add_index_field solr_name("license", :stored_searchable), helper_method: :license_links
     config.add_index_field solr_name("rtype", :stored_searchable), label: "Type", link_to_search: solr_name("rtype", :facetable)
-   # config.add_index_field solr_name("resource_type", :stored_searchable), label: "Resource Type", link_to_search: solr_name("resource_type", :facetable) -- removing to replace it with rtype.
+    # config.add_index_field solr_name("resource_type", :stored_searchable), label: "Resource Type", link_to_search: solr_name("resource_type", :facetable) -- removing to replace it with rtype.
     config.add_index_field solr_name("file_format", :stored_searchable), link_to_search: solr_name("file_format", :facetable)
     #config.add_index_field solr_name("identifier", :stored_searchable), helper_method: :index_field_link, field_name: 'identifier'
     config.add_index_field solr_name("identifier", :stored_searchable), helper_method: :iconify_auto_link, field_name: 'identifier'
