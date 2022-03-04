57f78fd468b287e25128cc512361e3127097d740
Mutugi Gathuri <mutugi.gathuri@mcgill.ca>
2022-03-04 15:43:41 -0500

    babel-plugin-transform-react-remove-prop-types

--- app/services/search_and_replace_in_field_of_object.rb
+++ app/services/search_and_replace_in_field_of_object.rb
@@ -6,22 +6,21 @@ class SearchAndReplaceInFieldOfObject
   @object = nil
 
   def initialize(search,replace,field,object)
-       raise ArgumentError.new("Missing required search argument.") if search.nil?
-       raise ArgumentError.new("Missing required replace argument.") if replace.nil?
-       raise ArgumentError.new("Missing required field argument.") if field.nil?
-       raise ArgumentError.new("Missing required object argument") if object.nil?
-       @search_val = search
-       @replace_val = replace
-       @field_name = field
-       @samvera_object = object
-       raise ArgumentError.new("Updates to the #{@field_name} field are not yet supported") if @field_name.include? 'creator'
-       search_and_replace_in_field_of_object
-     rescue ArgumentError => e
-       puts e.message
-     end
+      raise ArgumentError.new("Missing required search argument.") if search.nil?
+      raise ArgumentError.new("Missing required replace argument.") if replace.nil?
+      raise ArgumentError.new("Missing required field argument.") if field.nil?
+      raise ArgumentError.new("Missing required object argument") if object.nil?
+      @search_val = search
+      @replace_val = replace
+      @field_name = field
+      @samvera_object = object
+      raise ArgumentError.new("Updates to the #{@field_name} field are not yet supported") if @field_name.include? 'creator'
+      search_and_replace_in_field_of_object
+   rescue ArgumentError => e
+      puts e.message
    end
 
-  def search_and_replace_in_field_of_object
+   def search_and_replace_in_field_of_object
      if @samvera_object.attribute_names.include? @field_name
         if @samvera_object[@field_name].respond_to?(:each)
            new_field = Array.new
@@ -38,5 +37,5 @@ class SearchAndReplaceInFieldOfObject
         return false
      end
      @samvera_object
-  end
+   end
 end
