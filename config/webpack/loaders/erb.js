2ef4404fcf6bbd7da29c87552b31f39fd91ed65a
Mutugi Gathuri <mutugi.gathuri@mcgill.ca>
2022-03-04 14:41:56 -0500

    Fxing erb errors

--- config/webpack/loaders/erb.js
+++ config/webpack/loaders/erb.js
@@ -1,11 +1,12 @@
 module.exports = {
-  test: /\.erb$/,
-  enforce: 'pre',
-  exclude: /node_modules/,
-  use: [{
-    loader: 'rails-erb-loader',
-    options: {
-      runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
+    module: {
+      rules: [
+        {
+          test: /\.erb$/,
+          enforce: 'pre',
+          loader: 'rails-erb-loader'
+        },
+      ]
     }
-  }]
-}
+  }
+};
