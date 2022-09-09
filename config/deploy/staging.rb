9a611c9d5df26693066f33878be8f81b9dc1ff48
Mutugi Gathuri <mutugi.gathuri@mcgill.ca>
2022-09-09 15:11:52 -0400

    Fixing the path to the ssh keys for PROD/STAGING deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,5 +5,5 @@ set :rails_env, 'production'
 set :deploy_to, '/storage/www/murax'
 set :repository_cache, "git_cache"
 set :branch, "master"
-set :ssh_options, keys: ['id_rsa'] if File.exist?('id_rsa')
+set :ssh_options, { keys: '/root/.ssh/id_rsa', :forward_agent => true}
 server 'qlirap.library.mcgill.ca', user: 'dev.library@mcgill.ca', roles: [:web, :app, :db]
